79250ccbb31dedf528b12ea45611315c3656ec1c
gzhang <gzhang@empirix.com>
2019-07-11 18:08:36 -0400

    upgrade django from 1 to 2

--- Day19_review/learning_log/urls.py
+++ Day19_review/learning_log/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include, url
 
 urlpatterns = [
+    # url(r'^admin/', include(admin.site.urls)),
+    # url(r'', include('learning_logs.urls', namespace='learning_logs')),
     path('admin/', admin.site.urls),
+    path('', include('learning_logs.urls')),
 ]
