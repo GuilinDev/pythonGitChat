3627206a4c2b407914f7fb7a4a87f93333fd5f02
gzhang <gzhang@empirix.com>
2019-07-12 10:36:47 -0400

    Day20 Django Templates

--- Day19_review/learning_logs/urls.py
+++ Day19_review/learning_logs/urls.py
@@ -1,12 +1,23 @@
 """Defines url patterns for learning_logs."""
 
 from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
+app_name = 'learning_logs'
+
 urlpatterns = [
     # Home page.
-    url(r'^$', views.index, name='index'),
+    # url(r'^$', views.index, name='index'),
+    path('', views.index, name='index'),
+
+    # 显示所有的主题
+    # url(r'^topics/$', views.topics, name='topics'),
+    path('topics/', views.topics, name='topics'),
+
+    # Detail page for a single topic.
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
     
     # # Show all topics.
     # url(r'^topics/$', views.topics, name='topics'),
